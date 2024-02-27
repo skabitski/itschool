9f6d5f0c4e38ee4207c387e224e35e54f68d9e63
Maks_Skabitski <mskabitskiy@gmail.com>
2024-02-27 10:50:30 +0200

    test

---
+++ homework.js
@@ -0,0 +1,44 @@
+// let name = "Ilya";
+// alert(`hello ${1}`); // hello 1
+// alert(`hello ${"name"}`); //hello name
+// alert(`hello ${name}`); // hello Ilya
+
+// let x = 5;
+// let y = 2;
+// let z = x + y;
+// alert(z)   //7
+
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+// a = parseFloat(a);
+// b = parseFloat(b);
+// alert(a + b);
+
+// x = 5;
+// x++;
+// x -= 2;
+// alert(x);   //4
+
+// function sum(x, y=0) {
+//     return x+y;
+//   }
+//   alert(sum(5));   //sum(5, 0)
+
+// const testing = document.getElementById("buttonss");
+
+// function setName () {
+//     console.log()
+// setName()
+
+// }
+
+
+const button = document.getElementById("buttonss");
+
+    function setName() {
+        alert("whate your name");
+        alert("MAX")
+    }
+
+    button.addEventListener("click", setName);
+
